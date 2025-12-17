0e90abc248af181da1861bd03f25d5ce38cbaf8f
bala-amypo <bala@amypo.in>
2025-12-17 08:22:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertSchedule.java
+++ src/main/java/com/example/demo/entity/AlertSchedule.java
@@ -1,13 +1,61 @@
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.id;
+import jakarta.persistence.Unique;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
 
-
-
-
-
-
-
-
-
-
+@Entity
 public class AlertSchedule{
-    
+    @id
+@GeneratedValue(stategy=GenerationType.IDENTITY)
+    private long id;
+    private String warranty;
+    private String daysBeforeExpiry;
+    private String password;
+    private String role;
+
+public void getid(){
+    return id;
 }
+public void setid(long id){
+    this.id=id;
+}
+public String getname(){
+ return name;
+
+}
+public void setname(String name){
+    this.name=name;
+}
+
+public String getemail(){
+ return email;
+
+}
+public void setemail(String email){
+    this.email=email;
+}public String getpassword(){
+ return password;
+
+}
+public void setpassword(String password){
+    this.password=password;
+}public String getrole(){
+ return role;
+
+}
+public void setrole(String role){
+    this.role=role;
+}
+
+}
+
+
+
+
+
+
+
+
+
