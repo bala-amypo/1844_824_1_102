61436747f476b0863ac001cfa63391ca3a5fe319
bala-amypo <bala@amypo.in>
2025-12-17 06:47:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,7 +1,22 @@
 package com.example.semo.entity;
-import jakarta.persistence.Entity
+import jakarta.persistence.Entity;
+import jakarta.persistence.id;
+import jakarta.persistence.Unique;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
 
 @Entity
 public class Product{
+    @idGeneratedValues(strategy=GenerationType.IDENTITY)
+private Long id;
+private String  name;
+private String brand;
+private String modelNumber;
+private String category;
+
+
+
+
+
     
 }
