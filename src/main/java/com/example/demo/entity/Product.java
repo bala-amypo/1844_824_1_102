9e8dd29d0e007ee546da7b67b68d546779a32d41
bala-amypo <bala@amypo.in>
2025-12-17 06:52:07 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,7 +1,6 @@
 package com.example.semo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.id;
-import jakarta.persistence.Unique;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GeneratedType;
 
@@ -14,9 +13,39 @@ private String brand;
 private String modelNumber;
 private String category;
 
+public void getid(){
+    return id;
+}
+public void setid(long id){
+    this.id=id;
+}
+public String getname(){
+ return name;
+
+}
+public void setname(String name){
+    this.name=name;
+}
+
+public String getbrand(){
+ return brand;
+
+}
+public void setbrand(String brand){
+    this.brand=brand;
+}public String getmodelNumber(){
+ return modelNumber;
+
+}
+public void setmodelNumber(String modelNumber){
+    this.modelNumber=modelNumber;
+}public String getcategory(){
+ return category;
+
+}
+public void setcategory(String category){
+    this.category=category;
+}
 
 
-
-
-    
 }
