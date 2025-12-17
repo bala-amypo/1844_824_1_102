8c899b2f456b24e73d586375b7ec8d3bf55d0a5a
bala-amypo <bala@amypo.in>
2025-12-17 08:07:20 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -1,3 +1,65 @@
+package com.example.semo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedType;
+
+@Entity
 public class Warranty{
-    
+    @idGeneratedValues(strategy=GenerationType.IDENTITY)
+private Long id;
+private String  user;
+private String product;
+private String purchaseDate;
+private String expiryDate;
+private String serialNumber;
+
+public void getid(){
+    return id;
 }
+public void setid(long id){
+    this.id=id;
+}
+public String getname(){
+ return name;
+
+}
+public void setname(String name){
+    this.name=name;
+}
+
+public String getbrand(){
+ return brand;
+
+}
+public void setbrand(String brand){
+    this.brand=brand;
+}public String getmodelNumber(){
+ return modelNumber;
+
+}
+public void setmodelNumber(String modelNumber){
+    this.modelNumber=modelNumber;
+}public String getcategory(){
+ return category;
+
+}
+public void setcategory(String category){
+    this.category=category;
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
