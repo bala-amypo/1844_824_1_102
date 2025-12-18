6b9b53e776d65d749117bfbb9887b92427dd9bf4
bala-amypo <bala@amypo.in>
2025-12-18 08:13:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.collectiondb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.collectiondb.entity.studentEntity;
+import com.example.collectiondb.service.studentService;
+
+
+@RestController
+public class studentController {
+    
+    @Autowired
+    studentService stuser;
+
+    @PostMapping("/path")
+    public studentEntity studatas(@RequestBody studentEntity student) {
+        return stuser.saveData(student);
+    }
+    
+}
+
+
+studentController.java
---
+++ src/main/java/com/example/demo/service/WarrantyService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Warranty;
+
+@Service
+public class WarrantyService {
+   Warranty registerWarranty(Long userId,Long productId,Warranty warranty)
+   Warranty getWarranty(Long warrantyId)
+   List<Warranty
+    
+}
+
+
+
+
+
