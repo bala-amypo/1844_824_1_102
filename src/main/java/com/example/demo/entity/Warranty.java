c4a333029c18a3d3694842c96e3ad31a106595ca
bala-amypo <bala@amypo.in>
2025-12-18 04:59:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -1,78 +1,78 @@
-// package com.example.semo.entity;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import java.time.LocalDateTime;
-// @Entity
-// public class Warranty{
-//     @Id
-//    @GeneratedValue(strategy=GenerationType.IDENTITY)
-// private Long id;
-// private String  user;
-// private String product;
-// private LocalDateTime purchaseDate;
-// private LocalDateTime expiryDate;
-// private String serialNumber;
+package com.example.semo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+@Entity
+public class Warranty{
+    @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+private String  user;
+private String product;
+private LocalDateTime purchaseDate;
+private LocalDateTime expiryDate;
+private String serialNumber;
 
-// public Long getid(){
-//     return id;
-// }
-// public void setid(long id){
-//     this.id=id;
-// }
-// public String getuser(){
-//  return user;
+public Long getid(){
+    return id;
+}
+public void setid(long id){
+    this.id=id;
+}
+public String getuser(){
+ return user;
 
-// }
-// public void setuser(String user){
-//     this.user=user;
-// }
+}
+public void setuser(String user){
+    this.user=user;
+}
 
-// public String getproduct(){
-//  return product;
+public String getproduct(){
+ return product;
 
-// }
-// public void setproduct(String product){
-//     this.product=product;
+}
+public void setproduct(String product){
+    this.product=product;
 
-// }
-// public LocalDateTime getpurchaseDate(){
-//  return purchaseDate;
+}
+public LocalDateTime getpurchaseDate(){
+ return purchaseDate;
 
-// }
-// public void setpurchaseDate(LocalDateTime purchaseDate){
-//     this.purchaseDate=purchaseDate;
+}
+public void setpurchaseDate(LocalDateTime purchaseDate){
+    this.purchaseDate=purchaseDate;
 
-// }public LocalDateTime getexpiryDate(){
-//  return expiryDate;
+}public LocalDateTime getexpiryDate(){
+ return expiryDate;
 
-// }
-// public void setexpiryDate(LocalDateTime expiryDate){
-//     this.expiryDate=expiryDate;
-// }
-// public String getserialNumber(){
-//  return serialNumber;
+}
+public void setexpiryDate(LocalDateTime expiryDate){
+    this.expiryDate=expiryDate;
+}
+public String getserialNumber(){
+ return serialNumber;
 
-// }
-// public void setserialNumber(String serialNumber){
-//     this.serialNumber=serialNumber;
+}
+public void setserialNumber(String serialNumber){
+    this.serialNumber=serialNumber;
 
-// }
+}
 
-// public  Warranty(Long id, String  user,String product,LocalDateTime purchaseDate, LocalDateTime expiryDate,String serialNumber){
-// this.id=id;
-// this.user=user;
-// this.product=product;
-// this.purchaseDate=purchaseDate;
-// this.serialNumber=serialNumber;
-// this.expiryDate=expiryDate;
-// }
+public  Warranty(Long id, String  user,String product,LocalDateTime purchaseDate, LocalDateTime expiryDate,String serialNumber){
+this.id=id;
+this.user=user;
+this.product=product;
+this.purchaseDate=purchaseDate;
+this.serialNumber=serialNumber;
+this.expiryDate=expiryDate;
+}
 
-// public Warranty(){
-// }
+public Warranty(){
+}
 
-// }
+}
 
 
 
