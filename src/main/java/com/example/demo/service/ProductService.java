37983afbc9a668cd426093935a86928397f5d58a
bala-amypo <bala@amypo.in>
2025-12-18 06:56:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+
+@Service
+public class ProductService {
+   void Product addProduct(Product product);
+    list;
+   
+    
+}
+
+
+
+
+
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,8 +1,5 @@
 package com.example.demo.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
