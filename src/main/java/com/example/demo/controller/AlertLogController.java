879e4ad88123b283faaf971e10b064697eab6404
bala-amypo <bala@amypo.in>
2025-12-21 05:15:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/AlertLogController.java
+++ src/main/java/com/example/demo/controller/AlertLogController.java
@@ -1,31 +1,26 @@
 package com.example.demo.controller;
-
 import com.example.demo.entity.AlertLog;
 import com.example.demo.service.AlertLogService;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/alert")
 public class AlertLogController {
-
     private final AlertLogService service;
-
     public AlertLogController(AlertLogService service) {
+
         this.service = service;
     }
-
     @PostMapping
     public AlertLog create(@RequestBody AlertLog alert) {
         return service.createAlert(alert);
     }
-
     @GetMapping("/{id}")
     public AlertLog getById(@PathVariable Long id) {
         return service.getAlertById(id);
     }
-
+    
     @GetMapping
     public List<AlertLog> getAll() {
         return service.getAllAlerts();
