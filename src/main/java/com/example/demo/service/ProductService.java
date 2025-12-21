f2bd7023e24e8d191704d4494c67144f4dc42830
bala-amypo <bala@amypo.in>
2025-12-21 06:14:36 +0000

    User commit

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,23 +1,33 @@
-// package com.example.demo.service;
+package com.example.demo.service;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
 
-// import com.example.demo.entity.User;
+@Service
+public class ProductService {
 
-// @Service
-// public class ProductService {
+    private final ProductRepository repo;
 
-//    void Product addProduct(Product product);
-//     list;
-   
-    
-// 
-// /
-//  /}
-// 
+    public ProductService(ProductRepository repo) {
+        this.repo = repo;
+    }
 
+    public Product save(Product product) {
+        return repo.save(product);
+    }
 
+    public List<Product> getAll() {
+        return repo.findAll();
+    }
 
+    public Product getById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
 
-
-
+    public String delete(Long id) {
+        repo.deleteById(id);
+        return "Deleted Successfully";
+    }
+}
