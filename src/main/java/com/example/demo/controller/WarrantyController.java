1fb93f62e38067dbbe1a81087fd29edd4e8f1ba7
bala-amypo <bala@amypo.in>
2025-12-23 03:50:00 +0000

    User commit

--- src/main/java/com/example/demo/controller/WarrantyController.java
+++ src/main/java/com/example/demo/controller/WarrantyController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import com.example.demo.entity.Warranty;
 import com.example.demo.service.WarrantyService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping("/warranties")
