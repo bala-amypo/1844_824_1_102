f6cf5000854c4e81da02f6586cec925ba0443b66
bala-amypo <bala@amypo.in>
2025-12-26 16:52:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/WarrantyRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyRepository.java
@@ -10,6 +10,6 @@ public interface WarrantyRepository extends JpaRepository<Warranty, Long> {
     boolean existsBySerialNumber(String serialNumber);
 
     List<Warranty> findByUserId(Long userId);
-
+    List<Warranty> findByExpiryDateBetween(LocalDate start, LocalDate end);
     List<Warranty> findWarrantiesExpiringBetween(LocalDate from, LocalDate to);
 }
