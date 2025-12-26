8f35261613043e5bed8e280cbf51cb776f896f84
bala-amypo <bala@amypo.in>
2025-12-26 15:16:30 +0000

    User commit

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,10 +1,11 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Product;
+import java.util.List;
 
 public interface ProductService {
 
     Product addProduct(Product product);
 
-    Product getProductById(Long id);
+    List<Product> getAllProducts();   // 🔥 ADD THIS
 }
--- src/main/java/com/example/demo/service/WarrantyService.java
+++ src/main/java/com/example/demo/service/WarrantyService.java
@@ -1,17 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Warranty;
-
-import java.time.LocalDate;
 import java.util.List;
 
 public interface WarrantyService {
 
-    Warranty addWarranty(Warranty warranty);
+    Warranty registerWarranty(Long userId, Long productId, Warranty warranty);
 
-    Warranty getWarrantyById(Long id);
+    Warranty getWarranty(Long warrantyId);
 
-    List<Warranty> getWarrantiesByUser(Long userId);
-
-    List<Warranty> getWarrantiesExpiringBetween(LocalDate start, LocalDate end);
+    List<Warranty> getUserWarranties(Long userId);
 }
