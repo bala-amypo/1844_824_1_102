76e18d5be3b5f1b7c209315d6a4808d22de9fe16
bala-amypo <bala@amypo.in>
2025-12-26 05:03:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertLog.java
+++ src/main/java/com/example/demo/entity/AlertLog.java
@@ -6,53 +6,74 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.time.LocalDateTime;
 
+// @Entity
+// public class AlertLog {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private long id;
+//     private String warranty;
+//     private LocalDateTime sentAt;
+//     private String message;
+
+   
+//     public Long getId() {
+//         return id;
+//     }
+//     public void setId(long id) {
+//         this.id = id;
+//     }
+
+//     public String getWarranty() {
+//         return warranty;
+//     }
+//     public void setWarranty(String warranty) {
+//         this.warranty = warranty;
+//     }
+
+//     public LocalDateTime getSentAt() {
+//         return sentAt;
+//     }
+//     public void setSentAt(LocalDateTime sentAt) {
+//         this.sentAt = sentAt;
+//     }
+
+//     public String getMessage() {
+//         return message;
+//     }
+//     public void setMessage(String message) {
+//         this.message = message;
+//     }
+
+   
+//     public AlertLog(Long id, String warranty, LocalDateTime sentAt, String message) {
+//         this.id = id;
+//         this.warranty = warranty;
+//         this.sentAt = sentAt;
+//         this.message = message;
+//     }
+
+//     public AlertLog() {
+//     }
+// }
+
 @Entity
 public class AlertLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String warranty;
-    private LocalDateTime sentAt;
+    private Long alertId;
+
     private String message;
 
-   
-    public Long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
+    private LocalDateTime timestamp;
 
-    public String getWarranty() {
-        return warranty;
-    }
-    public void setWarranty(String warranty) {
-        this.warranty = warranty;
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;  // which user is affected OR created the action
 
-    public LocalDateTime getSentAt() {
-        return sentAt;
-    }
-    public void setSentAt(LocalDateTime sentAt) {
-        this.sentAt = sentAt;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-   
-    public AlertLog(Long id, String warranty, LocalDateTime sentAt, String message) {
-        this.id = id;
-        this.warranty = warranty;
-        this.sentAt = sentAt;
-        this.message = message;
-    }
-
-    public AlertLog() {
-    }
+    @ManyToOne
+    @JoinColumn(name = "request_id")
+    private ServiceRequest serviceRequest;  // related to which request
 }
+
