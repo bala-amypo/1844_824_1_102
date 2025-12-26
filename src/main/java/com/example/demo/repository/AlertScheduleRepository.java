959418146f6268fbbc4bb8fd4ffa52ccb9600784
bala-amypo <bala@amypo.in>
2025-12-26 04:51:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/AlertScheduleRepository.java
+++ src/main/java/com/example/demo/repository/AlertScheduleRepository.java
@@ -1,7 +1,19 @@
+// package com.example.demo.repository;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.example.demo.entity.AlertSchedule;
+
+// public interface AlertScheduleRepository extends JpaRepository<AlertSchedule, Long> {
+// }
+
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.AlertSchedule;
+import java.util.List;
 
 public interface AlertScheduleRepository extends JpaRepository<AlertSchedule, Long> {
+
+    List<AlertSchedule> findByWarranty(String warranty);
+
 }
