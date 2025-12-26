0a8c7fcc6c4f29c8e93034597ee0686a75d989ea
bala-amypo <bala@amypo.in>
2025-12-26 08:22:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,73 +1,29 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.util.List;
+import lombok.*;
+
+import java.util.Set;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
-    private String password;
+    private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
 
+    private String password;
+    private String role;
+
     @OneToMany(mappedBy = "user")
-    private List<Warranty> warranties;
-
-    // ------ GETTERS ------
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public List<Warranty> getWarranties() {
-        return warranties;
-    }
-
-    // ------ SETTERS ------
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setWarranties(List<Warranty> warranties) {
-        this.warranties = warranties;
-    }
-
-    // ------ CONSTRUCTORS ------
-    public User() {}
-
-    public User(Long id, String username, String password, String email) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
+    private Set<Warranty> warranties;
 }
