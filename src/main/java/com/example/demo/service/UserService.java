8a15cecca38c234ba55c67fd855f9a44042ef030
bala-amypo <bala@amypo.in>
2025-12-26 17:14:03 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.entity.User;
 
+
+import org.springframework.stereotype.Service; // 1. Ensure this is imported
+
+@Service // 2. ADD THIS ANNOTATION
+
 public interface UserService {
 
     User register(User user);
