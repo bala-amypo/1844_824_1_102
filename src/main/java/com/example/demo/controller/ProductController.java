088801840624d50503d67923e7409b9e0423eca6
bala-amypo <bala@amypo.in>
2025-12-26 06:29:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,42 +1,4 @@
-// package com.example.demo.controller;
-// import org.springframework.web.bind.annotation.*;
-// import java.util.List;
-// import com.example.demo.entity.Product;
-// import com.example.demo.service.ProductService;
 
-// @RestController
-// @RequestMapping("/products")
-// public class ProductController {
-
-//     private final ProductService service;
-
-//     public ProductController(ProductService service) {
-//         this.service = service;
-//     }
-
-   
-//     @PostMapping
-//     public Product create(@RequestBody Product product) {
-//         return service.save(product);
-//     }
-
-    
-//     @GetMapping
-//     public List<Product> getAll() {
-//         return service.getAll();
-//     }
-
-  
-//     @GetMapping("/{id}")
-//     public Product getById(@PathVariable Long id) {
-//         return service.getById(id);
-//     }
-
-//     @DeleteMapping("/{id}")
-//     public String delete(@PathVariable Long id) {
-//         return service.delete(id);
-//     }
-// }
 
 package com.example.demo.controller;
 
